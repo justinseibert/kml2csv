7334d4d1ffeeafad41ff636f28860c9520bd06a6
Justin Seibert <justinseibert@users.noreply.github.com>
2020-03-29 15:10:56 -0700

    enables style mapping

--- kml2csv.py
+++ kml2csv.py
@@ -17,11 +17,13 @@ class Kml2Csv:
     def __init__(self, options, inputFile):
         self.folders = []
         self.columns = []
+        self.styleColumns = []
         self.indexFolder = 0
         self.indexRow = 0
         self.inputFile = inputFile
         self.options = options
         self.rows = []
+        self.styleMap = {}
 
     def camelCase(self, text):
         text = text.replace('_', ' ')
@@ -34,35 +36,65 @@ class Kml2Csv:
         name = self.camelCase(name)
         return '{}{}'.format(prefix + '_' if bool(prefix) else '', name)
 
-    def parse_data(self, data, prefix, columns, level):
-        level += 1
+    def map_folder_columns(self, data, prefix, columns, level):
         if (not bool(data.contents)):
             return columns
 
+        level += 1
         key = data.name
         if (key == 'Data'):
             prefix = prefix + '_Data'
             key = data['name']
 
-        if (not self.options['styles'] and key in ['Style', 'StyleMap', 'styleUrl']):
-            return columns
-
         column_name = self.sanitize_column_name(prefix, key)
-        if (column_name == 'Folder_name'):
-            self.folders.append(data.string)
-            return columns
 
-        if (len(data.contents) > 1):
+        if (key == 'styleUrl' and not self.options['styles']):
+            pass
+        elif key == 'styleUrl':
+            style = data.string.replace('#','')
+            row = self.rows[self.indexFolder][self.indexRow]
+            self.rows[self.indexFolder][self.indexRow] = { **self.styleMap[style], **row }
+        elif (column_name == 'Folder_name'):
+            self.folders.append(data.string)
+        elif (len(data.contents) > 1):
             if level == 2:
                 self.rows[self.indexFolder].append({})
                 self.indexRow = len(self.rows[self.indexFolder]) - 1
             for item in data.contents:
                 if (item.name is not None):
-                    columns = self.parse_data(item, column_name, columns, level)
+                    columns = self.map_folder_columns(item, column_name, columns, level)
         else:
-            column = self.sanitize_column_name(prefix, key)
-            self.rows[self.indexFolder][self.indexRow][column] = data.string
-            if column not in columns:
+            column_name = self.sanitize_column_name(prefix, key)
+            self.rows[self.indexFolder][self.indexRow][column_name] = data.string
+            if column_name not in columns:
+                columns.append(column_name)
+
+        return columns
+
+    def map_style_columns(self, data, prefix, columns, level, key, variant):
+        level += 1
+        if (not bool(data.contents)):
+            return columns
+
+        col = data.name
+        if (level == 1):
+            key = data['id']
+            variant = key.split('-')[-1]
+            key = key.replace(f'-{variant}', '')
+            if not key in self.styleMap:
+                self.styleMap[key] = {}
+
+        column_name = self.sanitize_column_name(prefix, col)
+
+        if (len(data.contents) > 1):
+            for item in data.contents:
+                if (item.name is not None):
+                    columns = self.map_style_columns(item, column_name, columns, level, key, variant)
+        else:
+            column = f'{self.sanitize_column_name(prefix, col)}_{variant}'
+            self.styleMap[key][column] = data.string
+
+            if column not in self.styleColumns:
                 columns.append(column)
 
         return columns
@@ -71,13 +103,20 @@ class Kml2Csv:
         with open(self.inputFile, 'r') as f:
             kml = BeautifulSoup(f, 'xml')
 
+            if (self.options['styles']):
+                styles = kml.find_all('Style')
+                for index, style in enumerate(styles):
+                    styleColumns = self.map_style_columns(style, '', [], 0, '', '')
+                    self.styleColumns = [*styleColumns, *self.styleColumns]
+
             folders = kml.find_all('Folder')
             for index, folder in enumerate(folders):
-                self.indexFolder = index
                 self.indexRow = 0
-                self.columns.append([])
-                self.rows.append([{}])
-                self.columns[index] = self.parse_data(folder, '', [], 0)
+                columns = self.styleColumns if self.options['styles'] else []
+                self.columns.append(columns)
+                self.rows.append([])
+                self.columns[self.indexFolder] = self.map_folder_columns(folder, '', columns, 0)
+                self.indexFolder += 1
 
             for index, folder in enumerate(self.folders):
                 filename = '{}_{}.csv'.format(self.camelCase(folder), index)
